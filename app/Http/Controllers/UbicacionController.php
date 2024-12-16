f7af100abee856dc880e7e319be6a9e5d86de2dd
oscardariopb <cronos1610@hotmail.com>
2024-12-16 12:27:40 -0500

    Add Ubicacion with sede

--- app/Http/Controllers/UbicacionController.php
+++ app/Http/Controllers/UbicacionController.php
@@ -8,12 +8,21 @@ use App\Models\Ubicacion;
 class UbicacionController extends Controller
 {
     public function registerUbicacion(Request $request){
-        $request->validate(['nombre' => ['required', 'unique:ubicacions']]);
-        $ubicacion = Ubicacion::create(['nombre' => $request->nombre]); 
+        $request->validate([
+            'nombre'    => ['required', 'unique:ubicacions'],
+            'sede_id'   => ['required', 'numeric'],
+        ]);
+        $ubicacion = Ubicacion::create([
+            'nombre'    => $request->nombre,
+            'sede_id'   => $request->sede_id
+        ]);
         return response()->json(["msg" => "Registro exitoso", "res" => $ubicacion]); 
     }
 
-    public function getUbicaciones(Request $request){
+    public function getUbicaciones(Request $request) {
+        if ($request->sede_id) {
+            return Ubicacion::where('sede_id', $request->sede_id)->orderBy('nombre')->get();
+        }
         return Ubicacion::orderBy('nombre')->get();
     }
 }
