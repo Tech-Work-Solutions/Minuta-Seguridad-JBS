2e4bd643cb01c13b2530207c0bf683f7101faa9e
oscardariopb <cronos1610@hotmail.com>
2025-01-07 18:22:20 -0500

    New empty view for Hoja de vida

---
+++ resources/js/pages/admin/dashboard/recursosHumanos/Index.vue
@@ -0,0 +1,91 @@
+<template>
+    <div v-if="show">
+        <div class="flex flex-wrap items-center">
+            <h1 class="text-xl text-gray-500 pl-5 mr-5 font-bold">
+                <i class="fas fa-cogs"></i> Hoja de vida
+            </h1>
+        </div>
+        <br>
+        <div class="flex flex-wrap">
+            <div class="w-full">
+                <!-- Tabs Navigation -->
+                <ul class="flex mb-0 list-none flex-wrap pt-3 pb-4 flex-row">
+                    <li v-for="(tab, index) in tabs" :key="index"
+                        class="m-2 last:mr-0 flex-auto text-center cursor-pointer">
+                        <a class="text-sm font-bold px-5 py-3 shadow-lg rounded block leading-normal"
+                            :class="tabClasses(index + 1)" @click="toggleTabs(index + 1)">
+                            <em :class="tab.icon"></em> {{ tab.label }}
+                        </a>
+                    </li>
+                </ul>
+
+                <!-- Tab Content -->
+                <div class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded">
+                    <div class="flex-auto">
+                        <div class="tab-content tab-space">
+                            <div v-for="(tab, index) in tabs" :key="index"
+                                :class="{ 'hidden': openTab !== index + 1, 'block': openTab === index + 1 }">
+                                <component :is="tab.component" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import InformacionGeneral from './tabs/InformacionGeneral.vue';
+import InformacionPersonal from './tabs/InformacionPersonal.vue';
+import InformacionFamiliar from './tabs/InformacionFamiliar.vue';
+import EducacionAptitudes from './tabs/EducacionAptitudes.vue';
+import TrayectoriaEmpresas from './tabs/TrayectoriaEmpresas.vue';
+import ExperienciaLaboral from './tabs/ExperienciaLaboral.vue';
+import ReferenciasPersonales from './tabs/ReferenciasPersonales.vue';
+
+export default {
+    name: "tabs",
+    data() {
+        return {
+            openTab: 1,
+            show: false,
+            tabs: [
+                { label: 'Informacion General', icon: 'fas fa-car', component: 'InformacionGeneral' },
+                { label: 'Informacion Personal', icon: 'fas fa-user-shield', component: 'InformacionPersonal' },
+                { label: 'Informacion Familiar', icon: 'fas fa-globe', component: 'InformacionFamiliar' },
+                { label: 'Educacion y Aptitudes', icon: 'fas fa-caravan', component: 'EducacionAptitudes' },
+                { label: 'Trayectoria Empresas', icon: 'fas fa-hospital-alt', component: 'TrayectoriaEmpresas' },
+                { label: 'Experiencia Laboral', icon: 'fas fa-building', component: 'ExperienciaLaboral' },
+                { label: 'Referencias Personales', icon: 'fas fa-pen-fancy', component: 'ReferenciasPersonales' },
+            ],
+        };
+    },
+    mounted() {
+        const rol = localStorage.getItem('rol');
+        if (rol === 'GUARDA DE SEGURIDAD') {
+            this.$router.push('/dashboard');
+        }
+        this.show = true;
+    },
+    methods: {
+        toggleTabs(tabNumber) {
+            this.openTab = tabNumber;
+        },
+        tabClasses(tabNumber) {
+            return this.openTab === tabNumber
+                ? 'text-white bg-blue-500'
+                : 'text-gray-600 bg-white';
+        },
+    },
+    components: {
+        InformacionGeneral,
+        InformacionPersonal,
+        InformacionFamiliar,
+        EducacionAptitudes,
+        TrayectoriaEmpresas,
+        ExperienciaLaboral,
+        ReferenciasPersonales,
+    },
+};
+</script>
---
+++ resources/js/pages/admin/dashboard/recursosHumanos/tabs/InformacionGeneral.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <div class="flex flex-wrap items-center">
+            <h1 class="text-xl text-gray-500 pl-5 mr-5 font-bold"><em class="fas fa-file-signature"></em> Registro de
+                Informacion</h1>
+        </div>
+    </div>
+</template>
+
+<script>
+import { TRichSelect } from 'vue-tailwind/dist/components';
+
+export default {
+    data() {
+        return {
+            formData: {
+            },
+
+        };
+    },
+
+    async mounted() {
+    },
+
+    methods: {
+    },
+
+    validations: {
+        formData: {
+        }
+    },
+
+    computed: {
+
+    },
+
+    components: { TRichSelect }
+
+}
+</script>
