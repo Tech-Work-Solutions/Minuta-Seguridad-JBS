2177d457999d6fbe28213c3831b47fc1df8014c2
oscardariopb <cronos1610@hotmail.com>
2025-01-14 14:37:25 -0500

    Applying cr 1

--- resources/js/constants.js
+++ resources/js/constants.js
@@ -3,9 +3,9 @@ export const ROLES = {
     ADMINISTRATIVO: "ADMINISTRATIVO",
     ADMINISTRADOR: "ADMINISTRADOR",
 };
-  
+
 export const OPCIONES_MENU_GUARDA = ["minuta", "vehiculos", "visitantes"];
-  
+
 export const ICONOS_MAP = {
     minuta: "fa-file-signature",
     vehiculos: "fa-car",
@@ -14,9 +14,8 @@ export const ICONOS_MAP = {
     configuraciones: "fa-cogs",
     usuarios: "fa-users-cog",
 };
-  
-export const OPCIONES_MENU_ADMIN = [
 
+export const OPCIONES_MENU_ADMIN = [
     { label: "Minuta", route: "/minuta", icon: "fa-file-signature" },
     { label: "Vehiculos", route: "/vehiculos", icon: "fa-car" },
     { label: "Visitantes", route: "/visitantes", icon: "fa-users" },
@@ -24,4 +23,63 @@ export const OPCIONES_MENU_ADMIN = [
     { label: "Configuraciones", route: "/configuraciones", icon: "fa-cogs" },
     { label: "Usuarios", route: "/usuarios", icon: "fa-users-cog" },
     { label: "Hoja de vida", route: "/hojadevida", icon: "fa-paperclip" },
+];
+
+export const ACTIVIDADES_ECONOMICAS = [
+    { label: 'AGRICULTURA', key: 'agricultura' },
+    { label: 'GANADERÍA / AVICULTURA', key: 'ganaderia' },
+    { label: 'MINERÍA', key: 'mineria' },
+    { label: 'HIDROCARBUROS', key: 'hidrocarburos' },
+    { label: 'INDUSTRIA', key: 'industria' },
+    { label: 'Alimentos y Bebidas', key: 'alimentos' },
+    { label: 'Tabaco', key: 'tabaco' },
+    { label: 'Textiles y Confecciones', key: 'textiles' },
+    { label: 'Cuero y Calzado', key: 'cuerocalzado' },
+    { label: 'Papel y Cartón', key: 'papel' },
+    { label: 'Editorial y Artes Gráficas', key: 'editorial' },
+    { label: 'Químico y Farmacéutico', key: 'quimico' },
+    { label: 'Caucho y Plástico', key: 'caucho' },
+    { label: 'Vidrio, Cerámica y Cemento', key: 'vidrio' },
+    { label: 'Metalurgia', key: 'metalurgia' },
+    { label: 'Maquinaria', key: 'maquinaria' },
+    { label: 'Automotores', key: 'automotores' },
+    { label: 'Muebles', key: 'muebles' },
+    { label: 'Reciclaje', key: 'reciclaje' },
+    { label: 'ELECTRICIDAD / Gas / Agua', key: 'electricidad' },
+    { label: 'CONSTRUCCIÓN', key: 'construccion' },
+    { label: 'COMERCIO', key: 'comercio' },
+    { label: 'HOTELES Y RESTAURANTES', key: 'hoteles' },
+    { label: 'TRANSPORTE Y ALMACENAMIENTO', key: 'transporte' },
+    { label: 'COMUNICACIONES', key: 'comunicaciones' },
+    { label: 'FINANCIERO', key: 'financiero' },
+    { label: 'INMOBILIARIO', key: 'inmobiliario' },
+    { label: 'INFORMATICO', key: 'informatico' },
+    { label: 'SALUD', key: 'salud' },
+    { label: 'EDUCACION', key: 'educacion' },
+    { label: 'SEGUROS', key: 'seguros' },
+    { label: 'TURISMO / RECREACION', key: 'turismo' },
+    { label: 'OTROS SERVICIOS', key: 'otrosServicios' },
+    { label: 'Asesorías Profesionales', key: 'asesorias' },
+    { label: 'Servicios Temporales', key: 'servicios' },
+    { label: 'Seguridad Vigilancia', key: 'seguridad' },
+];
+
+export const AREAS_EMPRESA = [
+    { label: 'Administración', key: 'administracion' },
+    { label: 'Auditoría', key: 'auditoria' },
+    { label: 'Bodega', key: 'bodega' },
+    { label: 'Compras', key: 'compras' },
+    { label: 'Contabilidad', key: 'contabilidad' },
+    { label: 'Costos', key: 'costos' },
+    { label: 'Crédito y Cobranza', key: 'credito' },
+    { label: 'Diseño', key: 'diseño' },
+    { label: 'Finanzas', key: 'finanzas' },
+    { label: 'Gerencia General', key: 'gerencia' },
+    { label: 'Impuestos', key: 'impuestos' },
+    { label: 'Mercadeo', key: 'mercadeo' },
+    { label: 'Producción', key: 'produccion' },
+    { label: 'Publicidad', key: 'publicidad' },
+    { label: 'Personal', key: 'personal' },
+    { label: 'Sistemas', key: 'sistemas' },
+    { label: 'Tesorería', key: 'tesoreria' }
 ];
--- resources/js/pages/admin/dashboard/recursosHumanos/tabs/TrayectoriaEmpresas.vue
+++ resources/js/pages/admin/dashboard/recursosHumanos/tabs/TrayectoriaEmpresas.vue
@@ -1,11 +1,8 @@
 <template>
     <div class="bg-gray-100 p-6">
         <div class="max-w-5xl mx-auto bg-white shadow-md rounded-lg p-6">
-            <h1 class="text-xl font-bold text-center bg-blue-600 text-white p-2 rounded mb-4">
-                V. Trayectoria por Empresas
-            </h1>
             <p class="text-center mb-6 text-sm">
-                Marque con una equis (X) en qué clase de empresas ha trabajado y en qué área(s) de estas empresas se ha
+                Seleccione en qué clase de empresas ha trabajado y en qué área(s) de estas empresas se ha
                 desempeñado.
             </p>
 
@@ -15,7 +12,7 @@
                         <h2 class="text-lg font-semibold text-blue-600 mb-4">
                             Empresas por Actividades Económicas (*)
                         </h2>
-                        <div class="grid md:grid-cols-1 lg:grid-cols-2 gap-2">
+                        <div class="grid md:grid-cols-1 lg:grid-cols-3 gap-2">
                             <!-- Column 1 -->
                             <div>
                                 <label class="flex items-center" v-for="(tab, index) in opcionesActividades.firstColumn"
@@ -37,9 +34,35 @@
                                 </label>
 
                                 <label class="flex items-center">
-                                    Otros: <input type="text"
+                                    <input type="checkbox" class="mr-2"
+                                        v-model="formData.actividadesEconomicas.otrasEmpresas" />
+                                    OTRAS EMPRESAS (¿Cuáles?)
+                                </label>
+                                <label class="flex items-center">
+                                    <input type="text" :disabled="!formData.actividadesEconomicas.otrasEmpresas"
                                         class="ml-2 w-full py-2 px-3 border rounded-lg text-gray-700 focus:outline-none"
-                                        v-model="formData.actividadesEconomicas.otros" />
+                                        v-model="formData.actividadesEconomicas.empresasOpcionales" />
+                                </label>
+                            </div>
+
+                            <div>
+
+                                <label class="flex items-center" v-for="(tab, index) in opcionesActividades.thirdColumn"
+                                    :key="tab.label">
+                                    <input type="checkbox" class="mr-2"
+                                        v-model="formData.actividadesEconomicas[tab.key]" />
+                                    {{ tab.label }}
+                                </label>
+
+                                <label class="flex items-center">
+                                    <input type="checkbox" class="mr-2"
+                                        v-model="formData.actividadesEconomicas.otrosSectores" />
+                                    OTROS SECTORES
+                                </label>
+                                <label class="flex items-center">
+                                    <input type="text" :disabled="!formData.actividadesEconomicas.otrosSectores"
+                                        class="ml-2 w-full py-2 px-3 border rounded-lg text-gray-700 focus:outline-none"
+                                        v-model="formData.actividadesEconomicas.sectoresOpcionales" />
                                 </label>
                             </div>
                         </div>
@@ -66,9 +89,13 @@
                                     {{ tab.label }}
                                 </label>
                                 <label class="flex items-center">
-                                    Otros: <input type="text"
+                                    <input type="checkbox" class="mr-2" v-model="formData.areasEmpresa.otrasAreas" />
+                                    OTRAS (¿Cuáles?)
+                                </label>
+                                <label class="flex items-center">
+                                    <input type="text" :disabled="!formData.areasEmpresa.otrasAreas"
                                         class="ml-2 w-full py-2 px-3 border rounded-lg text-gray-700 focus:outline-none"
-                                        v-model="formData.areasEmpresa.otros" />
+                                        v-model="formData.areasEmpresa.areasOpcionales" />
                                 </label>
                             </div>
                         </div>
@@ -92,6 +119,7 @@
 
 <script>
 import { TRichSelect } from 'vue-tailwind/dist/components';
+import { ACTIVIDADES_ECONOMICAS, AREAS_EMPRESA } from '../../../../../constants';
 
 export default {
     props: {
@@ -137,7 +165,21 @@ export default {
                     'hoteles': false,
                     'transporte': false,
                     'comunicaciones': false,
-                    'otros': ''
+                    'financiero': false,
+                    'inmobiliario': false,
+                    'informatico': false,
+                    'salud': false,
+                    'educacion': false,
+                    'seguros': false,
+                    'turismo': false,
+                    'otrosServicios': false,
+                    'asesorias': false,
+                    'servicios': false,
+                    'seguridad': false,
+                    'otrosSectores': false,
+                    'otrasEmpresas': false,
+                    'empresasOpcionales': '',
+                    'sectoresOpcionales': '',
                 },
                 areasEmpresa: {
                     'administracion': false,
@@ -157,67 +199,40 @@ export default {
                     'personal': false,
                     'sistemas': false,
                     'tesoreria': false,
-                    'otros': ''
+                    'otrasAreas': false,
+                    'areasOpcionales': '',
                 }
             },
             opcionesActividades: {
-                firstColumn: [
-                    { label: 'AGRICULTURA', key: 'agricultura' },
-                    { label: 'GANADERÍA / AVICULTURA', key: 'ganaderia' },
-                    { label: 'MINERÍA', key: 'mineria' },
-                    { label: 'HIDROCARBUROS', key: 'hidrocarburos' },
-                    { label: 'INDUSTRIA', key: 'industria' },
-                    { label: 'Alimentos y Bebidas', key: 'alimentos' },
-                    { label: 'Tabaco', key: 'tabaco' },
-                    { label: 'Textiles y Confecciones', key: 'textiles' },
-                    { label: 'Cuero y Calzado', key: 'cuerocalzado' },
-                    { label: 'Papel y Cartón', key: 'papel' },
-                    { label: 'Editorial', key: 'editorial' },
-                    { label: 'Químico y Farmacéutico', key: 'quimico' },
-                    { label: 'Caucho y Plástico', key: 'caucho' },
-                    { label: 'Vidrio, Cerámica y Cemento', key: 'vidrio' },
-                ],
-                secondColumn: [
-                    { label: 'Metalurgia', key: 'metalurgia' },
-                    { label: 'Maquinaria', key: 'maquinaria' },
-                    { label: 'Automotores', key: 'automotores' },
-                    { label: 'Muebles', key: 'muebles' },
-                    { label: 'Reciclaje', key: 'reciclaje' },
-                    { label: 'ELECTRICIDAD / Gas / Agua', key: 'electricidad' },
-                    { label: 'CONSTRUCCIÓN', key: 'construccion' },
-                    { label: 'COMERCIO', key: 'comercio' },
-                    { label: 'HOTELES Y RESTAURANTES', key: 'hoteles' },
-                    { label: 'TRANSPORTE Y ALMACENAMIENTO', key: 'transporte' },
-                    { label: 'COMUNICACIONES', key: 'comunicaciones' },
-                ],
+                firstColumn: ACTIVIDADES_ECONOMICAS.slice(0, 14),
+                secondColumn: ACTIVIDADES_ECONOMICAS.slice(14, 25),
+                thirdColumn: ACTIVIDADES_ECONOMICAS.slice(25),
             },
             opcionesAreas: {
-                firstColumn: [
-                    { label: 'Administración', key: 'administracion' },
-                    { label: 'Auditoría', key: 'auditoria' },
-                    { label: 'Bodega', key: 'bodega' },
-                    { label: 'Compras', key: 'compras' },
-                    { label: 'Contabilidad', key: 'contabilidad' },
-                    { label: 'Costos', key: 'costos' },
-                    { label: 'Crédito y Cobranza', key: 'credito' },
-                    { label: 'Diseño', key: 'diseño' },
-                    { label: 'Finanzas', key: 'finanzas' },
-                    { label: 'Gerencia General', key: 'gerencia' },
-                    { label: 'Impuestos', key: 'impuestos' },
-                    { label: 'Mercadeo', key: 'mercadeo' },
-                    { label: 'Producción', key: 'produccion' },
-                    { label: 'Publicidad', key: 'publicidad' },
-                ],
-                secondColumn: [
-                    { label: 'Personal', key: 'personal' },
-                    { label: 'Sistemas', key: 'sistemas' },
-                    { label: 'Tesorería', key: 'tesoreria' }
-                ],
+                firstColumn: AREAS_EMPRESA.slice(0, 14),
+                secondColumn: AREAS_EMPRESA.slice(14),
             },
             submited: false,
             isUpdating: false,
             spiner: false,
         }
+    },
+    watch: {
+        "formData.actividadesEconomicas.otrosSectores"(newValue) {
+            if (newValue === false) {
+                this.formData.actividadesEconomicas.sectoresOpcionales = "";
+            }
+        },
+        "formData.actividadesEconomicas.otrasEmpresas"(newValue) {
+            if (newValue === false) {
+                this.formData.actividadesEconomicas.empresasOpcionales = "";
+            }
+        },
+        "formData.areasEmpresa.otrasAreas"(newValue) {
+            if (newValue === false) {
+                this.formData.areasEmpresa.areasOpcionales = "";
+            }
+        },
     },
 
     async mounted() {
