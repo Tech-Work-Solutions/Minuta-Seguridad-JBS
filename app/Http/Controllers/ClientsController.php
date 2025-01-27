ebf05397eeef442b994dfd6d3ce8de7c78d464c2
oscardariopb <cronos1610@hotmail.com>
2025-01-27 15:59:49 -0500

    remove img for clients

--- app/Http/Controllers/ClientsController.php
+++ app/Http/Controllers/ClientsController.php
@@ -7,43 +7,34 @@ use App\Models\Cliente;
 
 class ClientsController extends Controller
 {
-    public function registerClients(Request $request){
-        $request->validate([
-            'nit'  => ['required', 'numeric', 'unique:clientes'],
-            'nombre'            => ['required'],
-            'estado' => ['required', 'in:ACTIVO,INACTIVO,ELIMINADO'],
-            'img_header' => ['nullable', 'file', 'mimes:jpg,png'], 
-            'img_footer' => ['nullable', 'file', 'mimes:jpg,png'],
-        ]);
-        $img_header = null;
-        $img_footer = null;
+    public function registerClients(Request $request) {
+        try {
+            $request->validate([
+                'nit'               => ['required', 'numeric', 'unique:clientes'],
+                'nombre'            => ['required', 'string'],
+                'estado'            => ['required', 'in:ACTIVO,INACTIVO,ELIMINADO'],
+                'permisos_formulario' => ['required', 'array'],
+                'permisos_menu'     => ['required', 'array'],
+                'email'             => ['nullable', 'email'],
+            ]);
+            $client = Cliente::create([
+                'nit'       => $request->nit,
+                'nombre'    => $request->nombre,
+                'email'     => $request->email,
+                'permisos_menu'    => $request->permisos_menu,
+                'permisos_formulario'    => $request->permisos_formulario,
+                'estado'    => $request->estado
+            ]); 
+            return response()->json(["msg" => "Registro exitoso", "res" => $client]); 
+        } catch (\Illuminate\Validation\ValidationException $e) {
+            return response()->json([
+                'error' => 'Error de validación',
+                'messages' => $e->errors(),
+            ], 422);
 
-        if ($request->file('img_header')) {
-            $fileHeader = $request->file('img_header');
-            $nombreHeader = $fileHeader->getClientOriginalName();
-            $uniqueHeader = date("dmyHis") . '_header'; // Identificador único
-            $fileHeader->move(public_path() . '/img/clientes/', $uniqueHeader . $nombreHeader);
-            $img_header = '/img/clientes/' . $uniqueHeader . $nombreHeader;
-        } 
-
-        if ($request->file('img_footer')) {
-            $fileFooter = $request->file('img_footer');
-            $nombreFooter = $fileFooter->getClientOriginalName();
-            $uniqueFooter = date("dmyHis") . '_footer'; // Identificador único
-            $fileFooter->move(public_path() . '/img/clientes/', $uniqueFooter . $nombreFooter);
-            $img_footer = '/img/clientes/' . $uniqueFooter . $nombreFooter;
-        } 
-        $client = Cliente::create([
-            'nit'  => $request->nit,
-            'nombre'    => $request->nombre,
-            'email'    => $request->email,
-            'permisos_menu'    => $request->permisos_menu,
-            'permisos_formulario'    => $request->permisos_formulario,
-            'img_header'    => $img_header,
-            'img_footer'    => $img_footer,
-            'estado'    => $request->estado
-        ]); 
-        return response()->json(["msg" => "Registro exitoso", "res" => $client]); 
+        } catch (\Exception $e) {
+            return response()->json(["msg" => "Error en el registro", "error" => $e->getMessage()], 500);
+        }
     }
 
     public function getClients(Request $request){
