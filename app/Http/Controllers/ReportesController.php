eb4b697aeb93fff436c9f8bdb6c3570bcfff1c58
oscardariopb <cronos1610@hotmail.com>
2025-02-13 14:22:51 -0500

    Renombrar "Guarda" a "Usuario" en el controlador de reportes para mayor claridad y consistencia en la interfaz.

--- app/Http/Controllers/ReportesController.php
+++ app/Http/Controllers/ReportesController.php
@@ -59,7 +59,7 @@ class ReportesController extends Controller
             if($request->tipo === 'VEHICULO') { $column = 'vehicle_id'; }
             else if($request->tipo === 'CONDUCTOR') { $column = 'driver_id'; }
             else if($request->tipo === 'PROCEDENCIA') { $column = 'origin_id'; }
-            else if($request->tipo === 'GUARDA') { $column = 'user_id'; }
+            else if($request->tipo === 'USUARIO') { $column = 'user_id'; }
             else { $column = 'volqueta_id'; }
             $query->where($column, $request->value_id);
         }
@@ -189,7 +189,7 @@ class ReportesController extends Controller
             if($tipo === 'VEHICULO') { $column = 'vehicle_id'; }
             else if($tipo === 'CONDUCTOR') { $column = 'driver_id'; }
             else if($tipo === 'PROCEDENCIA') { $column = 'origin_id'; }
-            else if($tipo === 'GUARDA') { $column = 'user_id'; }
+            else if($tipo === 'USUARIO') { $column = 'user_id'; }
             else { $column = 'volqueta_id'; }
             $query->where($column, $value_id);
         }
