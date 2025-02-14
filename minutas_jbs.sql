8a1830573fb9acdc72b6da70e40e1ca2f6da9e22
oskr93 <oscare2193@hotmail.com>
2025-02-14 12:00:24 -0500

    add changes

--- minutas_jbs.sql
+++ minutas_jbs.sql
@@ -294,10 +294,10 @@ INSERT INTO `opciones_menu` (`id`, `nombre`, `estado`, `created_at`, `updated_at
 (4, 'Reportes', 'ACTIVO', '2024-12-05 17:33:34', NULL),
 (5, 'Configuraciones', 'ACTIVO', '2024-12-05 17:33:34', NULL),
 (6, 'Usuarios', 'ACTIVO', '2024-12-05 17:33:34', NULL);
-(8, 'Permisos', 'ACTIVO', '2024-02-03 13:33:34', NULL);
 (7, 'Turnos', 'ACTIVO', '2022-01-29 17:33:34', NULL);
-(7, 'Hoja De Vida', 'ACTIVO', '2025-01-19 17:33:34', NULL),
-(8, 'Gestionar Hoja de vida', 'ACTIVO', '2025-01-19 17:33:34', NULL);
+(8, 'Permisos', 'ACTIVO', '2024-02-03 13:33:34', NULL);(7, 'Hoja De Vida', 'ACTIVO', '2025-01-19 17:33:34', NULL),
+(9, 'Gestionar Hoja de vida', 'ACTIVO', '2025-01-19 17:33:34', NULL);
+(10, 'Hoja De Vida', 'ACTIVO', '2025-01-19 17:33:34', NULL),
 
 -- --------------------------------------------------------
 
@@ -1045,7 +1045,7 @@ ALTER TABLE `opciones_formulario`
 -- AUTO_INCREMENT de la tabla `opciones_menu`
 --
 ALTER TABLE `opciones_menu`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT de la tabla `origins`
